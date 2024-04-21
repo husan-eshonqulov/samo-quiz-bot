f576de610fd5cf6665cb9b6ae1b6cdf9eb9dfaaa
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2024-04-21 13:50:13 +0500

    add commands to bot

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -1,8 +1,12 @@
 import { Bot } from 'grammy';
 
 import config from '../config';
+import commands from './commands';
+import { addCommands } from '../utils';
 
 const bot = new Bot(config.botToken!);
+
+addCommands(commands, bot);
 
 bot.on('message:text', (ctx) => ctx.reply(ctx.message.text));
 
