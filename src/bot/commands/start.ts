9f6aaaf84103098e748a4a37895582d2a929f461
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2024-04-23 23:44:13 +0500

    switch to i18n

--- src/bot/commands/start.ts
+++ src/bot/commands/start.ts
@@ -1,7 +1,7 @@
 import MyContext from '../../types/context';
 
 const start = async (ctx: MyContext) => {
-  await ctx.reply('/start command typed!');
+  await ctx.reply(ctx.t('start-command'));
 };
 
 export default start;
