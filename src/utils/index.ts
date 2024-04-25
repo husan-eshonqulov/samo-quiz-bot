04afb04bda11ddc8fce1d38f18da411ddc5b565d
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2024-04-25 15:48:08 +0500

    legger merged to utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,9 @@
 import MyBot from '../types/bot';
 import Commands from '../types/commands';
 import MyMiddleware from '../types/middleware';
+import logger from './logger';
+
+export { logger };
 
 export const regCommands = (commands: Commands, bot: MyBot) => {
   for (const name in commands) {
