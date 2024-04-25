348adf0329ea6619205b3f0dd32d5c3ff7a4b30d
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2024-04-25 17:04:53 +0500

    change time format

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -53,7 +53,7 @@ if (process.env.NODE_ENV === 'production') {
     level: process.env.LOG_LEVEL || 'info',
     format: combine(
       errors({ stack: true }),
-      timestamp({ format: 'YYYY-MM-DD hh:mm:ss.SSS' }),
+      timestamp({ format: 'YYYY-MM-DD HH:mm:ss.SSS' }),
       align(),
       colorize({ all: true }),
       printf(
