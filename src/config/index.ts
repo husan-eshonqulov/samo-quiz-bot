9c3d7f3d37da5e90b66d56c5bc6951047ab8be8d
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2024-04-27 11:45:01 +0500

    add mongo uri

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,7 +3,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config = {
-  botToken: process.env.BOT_TOKEN
+  botToken: process.env.BOT_TOKEN,
+  mongoURI: process.env.MONGO_URI
 };
 
 export default config;
