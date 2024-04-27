7845a3006744b05d25ad1d6548cf8ee85c78d4e7
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2024-04-27 12:10:54 +0500

    connect db

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import bot from './bot';
-import { logger } from './utils';
+import config from './config';
+import { logger, connectDB } from './utils';
 
-const bootstrap = () => {
+const bootstrap = async () => {
+  try {
+    await connectDB(config.mongoURI!);
+    logger.info(`${config.mongoURI} has been connected...`);
+  } catch (error) {
+    logger.error(error);
+  }
+
   bot.start({
     onStart: () => {
       logger.info(`https://t.me/${bot.botInfo.username} has been started...`);
