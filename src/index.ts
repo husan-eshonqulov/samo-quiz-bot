56698e1eaf5356e773a6f5dc7adac0d3d08e7b95
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2024-05-01 00:30:54 +0500

    add bot.catch

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { GrammyError, HttpError } from 'grammy';
 import bot from './bot';
 import config from './config';
 import { logger, connectDB } from './utils';
@@ -13,6 +14,19 @@ const bootstrap = async () => {
   bot.start({
     onStart: () => {
       logger.info(`https://t.me/${bot.botInfo.username} has been started...`);
+    }
+  });
+
+  bot.catch((err) => {
+    const ctx = err.ctx;
+    logger.error(`Error while handling update ${ctx.update.update_id}`);
+    const e = err.error;
+    if (e instanceof GrammyError) {
+      logger.error('Error in request: ', e.description);
+    } else if (e instanceof HttpError) {
+      logger.error('Could not contact Telegram:', e);
+    } else {
+      logger.error('Unknown error:', e);
     }
   });
 };
