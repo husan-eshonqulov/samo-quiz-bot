2f3282cfdf75676378fc8704475c3526aecdd52d
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2024-04-30 21:46:12 +0500

    store sessions to mongodb

--- src/middlewares/session.ts
+++ src/middlewares/session.ts
@@ -1,9 +1,14 @@
 import { session } from 'grammy';
+import mongoose from 'mongoose';
+import { ISession, MongoDBAdapter } from '@grammyjs/storage-mongodb';
 
 import SessionData from '../types/session';
 
+const sessions = mongoose.connection.collection<ISession>('sessions');
+
 const mySession = session({
-  initial: (): SessionData => ({ __language_code: 'en' })
+  initial: (): SessionData => ({ __language_code: 'en' }),
+  storage: new MongoDBAdapter<SessionData>({ collection: sessions })
 });
 
 export default mySession;
