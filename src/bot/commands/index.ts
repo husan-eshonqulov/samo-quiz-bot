ceeb8be325b71a14971038199b565f46469835d2
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2024-05-04 10:08:01 +0500

    add test command

--- src/bot/commands/index.ts
+++ src/bot/commands/index.ts
@@ -1,10 +1,17 @@
+import Commands from '../../types/commands';
 import start from './start';
+import test from './test';
 
-const commands = [
+const commands: Commands = [
   {
     command: 'start',
     description: 'start command',
     method: start
+  },
+  {
+    command: 'test',
+    description: 'test command',
+    method: test
   }
 ];
 
